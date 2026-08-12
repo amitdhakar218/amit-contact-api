450c6cf9f3bb4c41f836459a8687b0440e1f099b
Amit Dhakar <amitdhakar218@gmail.com>
2026-08-12 16:05:16 +0530

    Added image, video, link support to email

--- api/send-email.js
+++ api/send-email.js
@@ -12,7 +12,7 @@ module.exports = async (req, res) => {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const { email, subject, message, attachmentUrl } = req.body;
+  const { email, subject, message, imageUrl, videoUrl, linkUrl } = req.body;
   if (!email || !message) {
     return res.status(400).json({ error: 'Missing fields' });
   }
@@ -27,9 +27,9 @@ module.exports = async (req, res) => {
     });
 
     let html = `<p><b>From:</b> ${email}</p><p><b>Subject:</b> ${subject || '(no subject)'}</p><p><b>Message:</b><br/>${message}</p>`;
-    if (attachmentUrl) {
-      html += `<p><b>Attachment:</b> <a href="${attachmentUrl}">${attachmentUrl}</a></p>`;
-    }
+    if (imageUrl) html += `<p><b>Image:</b><br/><a href="${imageUrl}">${imageUrl}</a><br/><img src="${imageUrl}" style="max-width:400px; margin-top:8px;" /></p>`;
+    if (videoUrl) html += `<p><b>Video:</b> <a href="${videoUrl}">${videoUrl}</a></p>`;
+    if (linkUrl) html += `<p><b>Link:</b> <a href="${linkUrl}">${linkUrl}</a></p>`;
 
     await transporter.sendMail({
       from: process.env.GMAIL_USER,
